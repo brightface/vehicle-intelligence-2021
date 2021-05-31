58ee8dee3efd4a34ed7e4a10503f8de5578e3944
brightface <brightface@hanmail.net>
2021-05-31 20:30:32 +0900

    assignment 5

--- code/week-6/GNB/classifier.py
+++ code/week-6/GNB/classifier.py
@@ -34,7 +34,39 @@ class GNB():
         for each class. Record them for later use in prediction.
         '''
         # TODO: implement code.
+        ### Collect the data ###
+        values_by_label = dict()
 
+        # INPUT FORMAT [ s, d, s_dot, d_dot ]
+        # for x,y in zip(X,Y):
+        #     print(x)
+        #     print(y)
+
+        for c in self.classes:
+            values_by_label[c] = np.empty((4, 0))
+        # Collect the data
+        # Dict ==> { class : [s : ], [d%4 : ], [s_dot : ], [d_dot : ]}
+        for x, y in zip(X, Y):
+            # item [s, d%4, s_dot, d_dot]
+            item = np.array([[x[0]], [(x[1] % 4)], [x[2]], [x[3]]])
+            # concat the array for get mean, stddevs
+            values_by_label[y] = np.append(values_by_label[y], item, axis=1)
+
+        # print(values_by_label)
+
+        ### Caculate mean and stddevs for each classes ###
+        means = dict()
+        stddevs = dict()
+
+        for c in self.classes:
+            class_array = np.array(values_by_label[c])
+            # get mean
+            means[c] = np.mean(class_array, axis=1)
+            # get stddevs
+            stddevs[c] = np.std(class_array, axis=1)
+
+        self.means = means
+        self.stddevs = stddevs
     # Given an observation (s, s_dot, d, d_dot), predict which behaviour
     # the vehicle is going to take using GNB.
     def predict(self, observation):
@@ -46,5 +78,26 @@ class GNB():
         Return the label for the highest conditional probability.
         '''
         # TODO: implement code.
-        return "keep"
+        # Calculate Gaussian probability for each variable
+        probs = dict()
+
+        for c in self.classes:
+            cur_prob = 1.00
+            # Multiply all the probabilities for variables
+            for idx in range(len(observation)):
+                cur_prob *= gaussian_prob(observation[idx], self.means[c][idx], self.stddevs[c][idx])
+
+            probs[c] = cur_prob
+
+        # Get hightest conditinal probability & label
+        highest_prob = 0
+        highest_class = "keep"
+
+        for c in self.classes:
+            if probs[c] > highest_prob:
+                highest_prob = probs[c]
+                highest_class = c
+
+        return highest_class
+
 
